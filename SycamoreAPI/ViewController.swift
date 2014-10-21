d73221bf8b15a082b6431c6e9f695b9bcc31bf67
Drew Rosenberg <drewrosenberg@gmail.com>
2014-10-20 22:04:17 -0700

    Initial Commit

---
+++ SycamoreAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SycamoreAPI
+//
+//  Created by Drew Rosenberg on 10/20/14.
+//  Copyright (c) 2014 Drew Rosenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
