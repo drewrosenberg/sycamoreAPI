ba0b05cdec94709e7b381610fe4bf47967d32309
Drew Rosenberg <drewrosenberg@gmail.com>
2014-11-02 09:26:00 -0800

    updated .gitignore

--- SycamoreAPI/Credentials.swift
+++ SycamoreAPI/Credentials.swift
@@ -7,15 +7,14 @@
 ////
 //
 
-
 //the client_id and client_secret can be obtained by signing up as a Sycamore developer at http://http://dev.sycamoreeducation.com/developer/
-let CLIENT_ID = <insert client secret>
-let CLIENT_SECRET = <insert client secret>
+let CLIENT_ID = "5445ed59b5755"
+let CLIENT_SECRET = "54c362a4e33dc9157540a2cd35ef0bc2"
 
 //this is the redirect_uri for the specific app
 //The URL scheme must be set in the target's info section
 //The app delegate must also be updated to receive these messages
-let REDIRECT_URI = Example:  "AppName://token_received"
+let REDIRECT_URI = "SycamoreAPI://token_received"
 
 
 
